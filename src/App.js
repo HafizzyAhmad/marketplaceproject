2d923c99dc4f706387d49222ffde6b9935d8b2b8
Hafizzy <hafizzy.ahmad@tm.com.my>
2022-02-11 12:13:35 +0800

    configure react router dom for pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Explore from './pages/Explore';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+// import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div>
-      <h1>Test APp</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore/>} />
+          <Route path='/forgotpassword' element={<ForgotPassword/>} />
+          <Route path='/offers' element={<Offers/>} />
+          <Route path='/profile' element={<SignIn/>} />
+          <Route path='/sign-in' element={<SignIn/>} />
+          <Route path='/sign-up' element={<SignUp/>} />
+        </Routes>
+        <Navbar/>
+      </Router>
+      {/* <h1>Test APp</h1> */}
+    </>
   );
 }
 
