0462d4022b54e0e0e911755d11f442fe5218c05b
Hafizzy <hafizzy.ahmad@tm.com.my>
2022-02-21 12:37:33 +0800

    Create UI and Logic for signin screen

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg';
+import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 
 const SignIn = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+
+  //destructure the data into one name
+  const {email, password} = formData;
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      //to get the value from all value that we define on the formData
+      [e.target.id]: e.target.value
+    }))
+  }
+
   return (
-    <div>
-      <h1>Sign In Page</h1>
-    </div>
+    <>
+      <div className='pageContainer'>
+        <header>
+          <p className='pageHeader'>Welcome Back!</p>
+        </header>
+        <main>
+          <form action="">
+            <input type="email" className='emailInput' placeholder='Email' id='email' value={email} onChange={onChange}/>
+            <div className="passwordInputDiv">
+              <input type={showPassword ? 'text' : 'password'}  className='passwordInput' placeholder='Password' id='password' value={password} onChange={onChange}/>
+              <img src={visibilityIcon} alt="showPassword" className="showPassword" onClick={() => setShowPassword((prevState) => !prevState)}/>
+            </div>
+            <Link to='/forgotpassword' className='forgotPasswordLink'>Forgot Password</Link>
+            <div className="signInBar">
+              <p className='signInText'>Sign In</p>
+              <button className='signInButton'><ArrowRightIcon fill='white' width='34px'/></button>
+            </div>
+          </form>
+          {/* Google OAuth Component */}
+          <Link to='/sign-up' className='registerLink'>
+            Sign Up Instead
+          </Link>
+        </main>
+      </div>
+    </>
   );
 }
 
